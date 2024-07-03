23526628b0f9e0b50016ad941dc95544dd1da7cc
Brainiac2009 <139386647+Brainiac2009@users.noreply.github.com>
2024-07-03 16:11:40 -0400

    Dashing
    Save to github

--- Final Project 1/Assets/Scripts/Dash.cs
+++ Final Project 1/Assets/Scripts/Dash.cs
@@ -5,25 +5,63 @@ using UnityEngine;
 public class Dash : MonoBehaviour
 {
     PlayerMovement moveScript;
+    EnemyMovement moveScriptEnemy;
 
     public float dashSpeed;
+
     public float dashTime;
 
+    public bool isPlayer1;
 
     // Start is called before the first frame update
     void Start()
     {
-        moveScript = GetComponent<PlayerMovement>();
+        moveScript =isPlayer1 ? GetComponent<PlayerMovement>() : null;
+        moveScriptEnemy = !isPlayer1 ? GetComponent<EnemyMovement>() : null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(Input.GetMouseButton(0))
-        (
-            StartCoroutine(Dash());
-        )
+        if (Input.GetKeyDown(KeyCode.S) && isPlayer1);  // Changed to GetMouseButtonDown for single click detection
+        {
+            StartCoroutine(DashCoroutine());  // Updated method name to DashCoroutine to avoid confusion with class name
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && !isPlayer1);  // Changed to GetMouseButtonDown for single click detection
+        {
+            StartCoroutine(DashCoroutine());  // Updated method name to DashCoroutine to avoid confusion with class name
+        }
+    }
 
+    IEnumerator DashCoroutine()  // Updated method name to DashCoroutine to avoid confusion with class name
+    {
+        float startTime = Time.time;
+
+        while (Time.time < startTime + dashTime)  // Changed While to while
+        {
+            if (isPlayer1)
+            {
+                if (moveScript.isFacingRight){
+                moveScript.GetComponent<CharacterController>().Move(new Vector3(0,0,1) * dashSpeed * Time.deltaTime);
+                }
+                else 
+                {
+                    moveScript.GetComponent<CharacterController>().Move(new Vector3(0,0,-1) * dashSpeed * Time.deltaTime);
+
+                }
+            }
+            else
+            {
+                if (moveScriptEnemy.isFacingRight){
+                    moveScriptEnemy.GetComponent<CharacterController>().Move(new Vector3(0,0,1) * dashSpeed * Time.deltaTime);
+                }
+                else 
+                {
+                    moveScriptEnemy.GetComponent<CharacterController>().Move(new Vector3(0,0,-1) * dashSpeed * Time.deltaTime);
+
+                }
+                yield return null;  // Corrected 'yeild' to 'yield'
+            }
+        }
     }
 }
